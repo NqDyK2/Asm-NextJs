dab558b36143ea32d84515db8be5df8e8b6a7459
Khoa <khoa10688@gmail.com>
2022-07-26 14:14:35 +0700

    create-header-component

---
+++ src/components/Header/data-menu.tsx
@@ -0,0 +1,6 @@
+export const MenuList = [
+    { id: 1, label: "Trang chủ", path: "/" },
+    { id: 2, label: "Phim T.hinh", path: "/" },
+    { id: 3, label: "Phim", path: "/" },
+
+]
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import React from 'react'
+import Banner from '../Banner'
+import styles from './header.module.css'
+
+import {MenuList} from './data-menu'
+
+type Props = {}
+
+const Header = (props: Props) => {
+    return (
+        <div>
+            <header className={styles.header}>
+                <div className={styles.netflixLogo}>
+                    <Link className={styles.logo} href="/"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image" /></Link>
+                </div>
+                <nav className={styles.main_nav}>
+                    {MenuList.map ((menu) =>(
+                        <Link className={styles.Link} href= {menu.path}> 
+                            <a className={styles.aLink}>
+                                {menu.label}
+                            </a>
+                         </Link>
+                    ))}
+                </nav>
+                <nav className={styles.sub_nav}>
+                    <Link href="#"><i className="fas fa-search sub-nav-logo" /></Link>
+                    <Link href="#"><i className="fas fa-bell sub-nav-logo" /></Link>
+                    <Link href="#">Account</Link>
+                </nav>
+            </header>
+
+        </div>
+
+    )
+}
+
+export default Header
