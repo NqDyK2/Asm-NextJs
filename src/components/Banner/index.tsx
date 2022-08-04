312fc50a85b8e068c4e183c3d6f2921b26f7bd93
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-08-04 18:40:29 +0700

    NEXT-7 map API banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,16 +8,16 @@ import "swiper/css/navigation";
 import useFilms from '@/hooks/use-film';
 
 const Banner = () => {
-  const { data:films, error } = useFilms();
+  const { data: films, error } = useFilms();
   if (error) return <div>failed to load...</div>
   if (!films) return <div> loading...</div>
   console.log('films', films);
   // Object.values(data).map((x) => console.log(x))
   return (
     <div>
-      { films.map((item:any) => {
+      {/* {films.map((item: any) => {
         return <div key={item.id}><li>{item.name}</li></div>
-      })}
+      })} */}
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -32,23 +32,25 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className={styles.mySwiper}
       >
-        <SwiperSlide className={styles.swiper_slide}>
-          <div className={styles.item_banner}>
-            <div className={styles.item}>
-              <div className={styles.desc}>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
-                </p>
-              </div>
-              <div className={styles.btn_handle_play}>
-                <button className={styles.btn_play}> Phát</button>
-                <button className={styles.btn_info} >Thông tin khác</button>
+        {films.map((item: any, index:any) => {
+            return<SwiperSlide className={styles.swiper_slide}>
+             <div key={index} className={styles.item_banner}>
+              <div className={styles.item}>
+                <div className={styles.desc}>
+                  <p>
+                    {item.overview}
+                  </p>
+                </div>
+                <div className={styles.btn_handle_play}>
+                  <button className={styles.btn_play}> Phát</button>
+                  <button className={styles.btn_info} >Thông tin khác</button>
+                </div>
               </div>
             </div>
-          </div>
-          <img src="https://rare-gallery.com/thumbnail/44781-Po-Kung-Fu-PandaKung-Fu-Panda-3-HD-Wallpaper.jpg" alt="" />
-        </SwiperSlide>
+              <img src={item.poster_path} alt="" />
 
+          </SwiperSlide>
+          })}
         {/* <SwiperSlide className={styles.swiper_slide}>
           <img src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg" alt="" /></SwiperSlide>
         <SwiperSlide className={styles.swiper_slide}>
