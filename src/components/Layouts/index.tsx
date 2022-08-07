f3a2e13217197916afbe66830761dfadf373bf13
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-08-08 02:16:41 +0700

    update search

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className='bg-black' >
       <Header />
-      <div className='mt-40'>
+      <div className=''>
         {children}
       </div>
       <Footer />
